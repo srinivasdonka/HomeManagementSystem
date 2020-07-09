1962b270340417b0282ae982a98116721d19f8d4
srinu <srinu@DESKTOP-17AD6AP>
2020-07-09 13:45:30 +0530

    Updated constant keys

---
+++ HomeManagement/src/main/java/com/homemanagement/constant/HomeManagementKeyConstant.java
@@ -0,0 +1,64 @@
+package com.homemanagement.constant;
+
+public class HomeManagementKeyConstant {
+
+
+	private HomeManagementKeyConstant() {
+
+	}
+	public static final String  USERNAME="username";
+	public static final String  USER_NAME="user_name";
+	public static final String  PASS_KEY="password";
+	public static final String  FIRSTNAME="first_name";
+	public static final String  LASTNAME="last_name";
+	public static final String  DESIGNATION="designation";
+	public static final String  ROLLID="role_id";
+	public static final String  PHONE="phone";
+	public static final String  STATUS="status";
+	public static final String  UPDATETIMESTAMP="upated_timestamp";
+	public static final String  LASTLOGIN="lastLogin";
+	public static final String  ID="id";
+	public static final String  ACTIVE="Active";
+	public static final String  EMAILTOKEN="email_Token";
+	public static final String  USERID="user_id";
+	public static final String  CREATEDATE="created_date";
+	public static final String  USERHISTORY="user_history";
+	public static final String  LOGDATA="log_data";
+	public static final String  COMPANYID="company_id";
+	public static final String  SERIALNUMBER="serial_number";
+	public static final String ISACTIVE="is_active";
+	public static final String DEVICETOKEN="device_token";
+	public static final String COMPANYNAME="company_name";
+	public static final String LASTUPDATE="last_updated";
+	public static final String  NOTIFICATION_MSG="notification_msg";
+	public static final String  XADA_Administrator="XADA Administrator";
+	public static final String  Administrator="Administrator";
+	public static final String  Enduser="Enduser";
+	
+	//Custom Messages
+	public static final String SUCCESS="success";
+	public static final String MESSAGE="success";
+	public static final String FAILURE="failure";
+	public static final String  IN_ACTIVE="Inactive";
+	public static final String ACTIVATE_PENDING="Activation Pending";
+	public static final String UPDATED_SUCCESSFULLY="successfully updated Device Details";
+	public static final String REGISTERED_SUCCESSFULLY="successfully registered";
+
+
+	//error msgs
+	public static final String INVALID_PAYLOAD="Invalid payload";
+	public static final String DATAINTGRTY_VIOLATION="DATAINTGRTY_VIOLATION";
+	public static final String SLASH = "/";
+	public static final String DOT = ".";
+	public static final String URL_PATH = "/ravenhistory/logs";
+	public static final String RAVEN_LOG = "/raven_application.log";
+	public static final String REG_EXP = "[\\+\\^{}^\\\"']";
+	public static final String DOT_REG_EXP = "(.*?)" + ":" + "(.*)";
+	public static final String EMPTY_MSG="Message is empty";
+	public static final String UPLOAD_PKG="Package uploaded successfully";
+	
+	public static final String  ANONYMOUS_USER="anonymousUser";
+	
+
+
+}
